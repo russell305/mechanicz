6b67a95154c89eb4dc2163888b6c40195afff549
Russell Mckee <russm305@gmail.com>
2019-02-23 13:38:17 -0500

    complete full stack implementation

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ from pprint import pprint
 import json #for Python to Javascript
 import requests #for JSON
 
+
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
 GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -28,12 +29,17 @@ db = scoped_session(sessionmaker(bind=engine)) # for individual sessions
 
 mechanic_list=[]
 
-#db.execute("CREATE TABLE mechanic(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, phone VARCHAR NOT NULL, address VARCHAR NOT NULL, latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, email VARCHAR, oil_change SMALLINT NOT NULL, battery SMALLINT NOT NULL, pads_front SMALLINT NOT NULL, pads_back SMALLINT NOT NULL, starting_problem SMALLINT NOT NULL, check_engine SMALLINT NOT NULL, tune_up SMALLINT NOT NULL, starter SMALLINT NOT NULL, alternator SMALLINT NOT NULL, spark_plugs SMALLINT NOT NULL, valve_cover SMALLINT NOT NULL, air_filter SMALLINT NOT NULL, mobile_mechanic BOOLEAN NOT NULL, air_conditioning BOOLEAN NOT NULL, auto_body BOOLEAN NOT NULL, tire_rotation BOOLEAN NOT NULL, fix_flat BOOLEAN NOT NULL, car_wash BOOLEAN NOT NULL)")
+
+#db.execute("CREATE TABLE mechanic_list1(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, password VARCHAR NOT NULL, phone VARCHAR NOT NULL, address VARCHAR NOT NULL, latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, email VARCHAR, years SMALLINT NOT NULL, description VARCHAR NOT NULL, oil_change SMALLINT NOT NULL, battery SMALLINT NOT NULL, pads_front SMALLINT NOT NULL, pads_back SMALLINT NOT NULL, starting_problem SMALLINT NOT NULL, check_engine SMALLINT NOT NULL, tune_up SMALLINT NOT NULL, starter SMALLINT NOT NULL, alternator SMALLINT NOT NULL, spark_plugs SMALLINT NOT NULL, valve_cover SMALLINT NOT NULL, air_filter SMALLINT NOT NULL, mobile_mechanic BOOLEAN NOT NULL, air_conditioning BOOLEAN NOT NULL, auto_body BOOLEAN NOT NULL, tire_rotation BOOLEAN NOT NULL, fix_flat BOOLEAN NOT NULL, car_wash BOOLEAN NOT NULL)")
 #db.commit()
-mechanicsD = db.execute("SELECT * FROM mechanic").fetchall()
-print("mechanicsD",mechanicsD)
+
+
+
 @app.route("/", methods = ["GET"]) # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
+	mechanicsD = db.execute("SELECT * FROM mechanic_list1").fetchall()
+	print("mechanicsD",mechanicsD)
+	print("Databasesuccess")
 	for i in mechanicsD:
 		print("mechanicsD",i)
 		mechanic_data = {
@@ -43,10 +49,113 @@ def index():
 			"longitude": i.longitude,
 			"latitude": i.latitude,
 			"email": i.email,
+			"years": i.years,
+			"description": i.description,
+			"oil_change": i.oil_change,
+			"battery": i.battery,
+			"pads_front": i.pads_front,
+			"pads_back": i.pads_back,
+			"starting_problem": i.starting_problem,
+			"check_engine": i.check_engine,
+			"tune_up": i.tune_up,
+			"starter": i.starter,
+			"alternator": i.alternator,
+			"spark_plugs": i.spark_plugs,
+			"valve_cover": i.valve_cover,
+			"air_filter": i.air_filter,
+			"mobile_mechanic": i.mobile_mechanic,
+			"air_conditioning": i.air_conditioning,
+			"auto_body": i.auto_body,
+			"tire_rotation": i.tire_rotation,
+			"fix_flat": i.fix_flat,
+			"car_wash": i.car_wash,
 			}
+		print ("description",i.description)
+		print ("years",i.years)
 		mechanic_list.append(mechanic_data)
+
 	return render_template("index.html", mechanic_list=mechanic_list)
 
 @app.route("/signup", methods = ["POST"])
 def signup():
 	return render_template("signup.html")
+
+@app.route("/signup_check", methods = ["POST"])
+def signup_check():
+	name = request.form.get("name")
+	password = request.form.get("password")
+	phone = request.form.get("phone")
+	street = request.form.get("street")
+	city = request.form.get("city")
+	state = request.form.get("state")
+	zip_code = request.form.get("zip_code")
+	email = request.form.get("email")
+	years = request.form.get("years")
+	description = request.form.get("description")
+	oil_change = request.form.get("oil_change")
+	battery = request.form.get("battery")
+	pads_front = request.form.get("pads_front")
+	pads_back = request.form.get("pads_back")
+	starting_problem= request.form.get("starting_problem")
+	check_engine = request.form.get("check_engine")
+	tune_up = request.form.get("tune_up")
+	starter = request.form.get("starter")
+	alternator = request.form.get("alternator")
+	spark_plugs = request.form.get("spark_plugs")
+	valve_cover = request.form.get("valve_cover")
+	air_filter = request.form.get("air_filter")
+	mobile_mechanic = request.form.get("mobile_mechanic")
+	air_conditioning = request.form.get("air_conditioning")
+	auto_body = request.form.get("auto_body")
+	tire_rotation = request.form.get("tire_rotation")
+	fix_flat = request.form.get("fix_flat")
+	car_wash = request.form.get("car_wash")
+	address = street+", "+city+", "+state+", "+zip_code
+	print(address)
+
+	if mobile_mechanic=="on":
+		mobile_mechanic=True
+	else:
+		mobile_mechanic=False
+
+	if air_conditioning=="on":
+		air_conditioning=True
+	else:
+		air_conditioning=False
+
+	if auto_body=="on":
+		auto_body=True
+	else:
+		auto_body=False
+
+	if fix_flat=="on":
+		fix_flat=True
+	else:
+		fix_flat=False
+
+	if tire_rotation=="on":
+		tire_rotation=True
+	else:
+		tire_rotation=False
+
+	if car_wash=="on":
+		car_wash=True
+	else:
+		car_wash=False
+
+		#geocode
+	params = {
+		'address': address,
+		'key': 'AIzaSyD9fytSdXXr6kVZdXLddFJyF9HT4JTt-qM',
+	}
+	res = requests.get(GOOGLE_MAPS_API_URL, params=params)
+	response = res.json()
+	#print("jsonresonse",response)
+	latlng=response['results'][0]['geometry']['location']
+	latitude = latlng['lat']
+	longitude = latlng['lng']
+	print("lat", latlng['lat'])
+	print("lng", latlng['lng'])
+	db.execute("INSERT INTO mechanic_list1 (name, password, phone, address, latitude, longitude, email, years, description, oil_change, battery, pads_front, pads_back, starting_problem, check_engine, tune_up, starter, alternator, spark_plugs, valve_cover, air_filter, mobile_mechanic, air_conditioning, auto_body, tire_rotation, fix_flat, car_wash) VALUES (:name, :password, :phone, :address, :latitude, :longitude, :email, :years, :description, :oil_change, :battery, :pads_front, :pads_back, :starting_problem, :check_engine, :tune_up, :starter, :alternator, :spark_plugs, :valve_cover, :air_filter, :mobile_mechanic, :air_conditioning, :auto_body, :tire_rotation, :fix_flat, :car_wash)", {"name":name, "password":password, "phone":phone, "address":address, "latitude":latitude, "longitude":longitude, "email":email, "years":years, "description":description, "oil_change":oil_change, "battery":battery, "pads_front":pads_front, "pads_back":pads_back, "starting_problem":starting_problem, "check_engine":check_engine, "tune_up":tune_up, "starter":starter, "alternator":alternator, "spark_plugs":spark_plugs, "valve_cover":valve_cover, "air_filter":air_filter, "mobile_mechanic":mobile_mechanic, "air_conditioning":air_conditioning, "auto_body":auto_body, "tire_rotation":tire_rotation, "fix_flat":fix_flat, "car_wash":car_wash})
+	db.commit()
+	return "Uploaded to Database"
